835c69de9756788cf50df624e9ffe106d2dd6581
ongaeshi <ongaeshi0621@gmail.com>
2021-10-24 13:07:45 +0900

    feat: ".gif"のときだけ読み込む

--- main.rb
+++ main.rb
@@ -17,9 +17,12 @@ end
 App.run do
   if DragDrop.has_new_file_paths
     gif_path = DragDrop.get_dropped_file_path
-    $gif_reader = GifReader.new(gif_path)
-    App.window_size($gif_reader.width * SCALE, $gif_reader.height * SCALE)
-    App.end_time = $gif_reader.duration
-    App.reset
+
+    if gif_path.end_with?(".gif")
+      $gif_reader = GifReader.new(gif_path)
+      App.window_size($gif_reader.width * SCALE, $gif_reader.height * SCALE)
+      App.end_time = $gif_reader.duration
+      App.reset
+    end
   end
 end
